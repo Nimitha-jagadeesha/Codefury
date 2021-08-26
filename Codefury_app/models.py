c4761820b930538ecfb05eda2d13ee743571688c
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-08-26 20:49:44 +0530

    created Contacts model

--- Codefury_app/models.py
+++ Codefury_app/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Announcment(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     title = models.TextField() 
+
+class Contact(models.Model):
+    email= models.EmailField(blank = False)
+    message= models.TextField()
     
