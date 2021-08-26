570cefd5f03420be88c23bde3899b66eac77e663
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-08-26 23:32:06 +0530

    changed model structure and view function
    Created a new model of FAQ questions and made changes to the existing Announcement section

--- Codefury_app/models.py
+++ Codefury_app/models.py
@@ -1,11 +1,24 @@
 from django.db import models
-from django.utils import timezone
+
 # Create your models here.
 class Announcment(models.Model):
-    date = models.DateTimeField(auto_now_add=True)
-    title = models.TextField() 
+    title = models.CharField(max_length=300, blank=True)
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 class Contact(models.Model):
-    email= models.EmailField(blank = False)
-    message= models.TextField(max_length=300)
+    email = models.EmailField(blank = False)
+    message = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.email
     
+class Question(models.Model):
+    question = models.TextField(max_length = 100)
+    answer = models.TextField(max_length = 400)
+
+    def __str__(self):
+        return self.question
