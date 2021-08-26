6e0245539660a3c133f7d0a9cb331222c820dd0a
Poojari-Prajwal <84854216+Poojari-Prajwal@users.noreply.github.com>
2021-08-26 18:21:04 +0530

    created models and migrated

--- Codefury_app/models.py
+++ Codefury_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Announcment(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    title = models.TextField() 
+    
