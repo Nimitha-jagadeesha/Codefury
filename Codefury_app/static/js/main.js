6556ef1856a727e424ef89ddb80f72ca6a3822e8
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-09-03 07:48:48 +0530

    removed vendor files and added CDNS
    removed unwanted vendor files and added CDNs, this will greatly improve performance.
    Also altered a couple of styles.

--- Codefury_app/static/js/main.js
+++ Codefury_app/static/js/main.js
@@ -1,150 +1,162 @@
-
-(function() {
+(function () {
   "use strict";
 
   /**
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim()
+    el = el.trim();
     if (all) {
-      return [...document.querySelectorAll(el)]
+      return [...document.querySelectorAll(el)];
     } else {
-      return document.querySelector(el)
+      return document.querySelector(el);
     }
-  }
+  };
 
   /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+    let selectEl = select(el, all);
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach((e) => e.addEventListener(type, listener));
       } else {
-        selectEl.addEventListener(type, listener)
+        selectEl.addEventListener(type, listener);
       }
     }
-  }
+  };
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+    el.addEventListener("scroll", listener);
+  };
 
   /**
    * Navbar links active state on scroll
    */
-  let navbarlinks = select('#navbar .scrollto', true)
+  let navbarlinks = select("#navbar .scrollto", true);
   const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
+    let position = window.scrollY + 200;
+    navbarlinks.forEach((navbarlink) => {
+      if (!navbarlink.hash) return;
+      let section = select(navbarlink.hash);
+      if (!section) return;
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
+        navbarlink.classList.add("active");
       } else {
-        navbarlink.classList.remove('active')
+        navbarlink.classList.remove("active");
       }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
+    });
+  };
+  window.addEventListener("load", navbarlinksActive);
+  onscroll(document, navbarlinksActive);
 
   /**
    * Scrolls to an element with header offset
    */
   const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
+    let header = select("#header");
+    let offset = header.offsetHeight;
 
-    let elementPos = select(el).offsetTop
+    let elementPos = select(el).offsetTop;
     window.scrollTo({
       top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
+      behavior: "smooth",
+    });
+  };
 
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
-  let selectHeader = select('#header')
+  let selectHeader = select("#header");
   if (selectHeader) {
     const headerScrolled = () => {
       if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
+        selectHeader.classList.add("header-scrolled");
       } else {
-        selectHeader.classList.remove('header-scrolled')
+        selectHeader.classList.remove("header-scrolled");
       }
-    }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
+    };
+    window.addEventListener("load", headerScrolled);
+    onscroll(document, headerScrolled);
   }
 
   /**
    * Back to top button
    */
-  let backtotop = select('.back-to-top')
+  let backtotop = select(".back-to-top");
   if (backtotop) {
     const toggleBacktotop = () => {
       if (window.scrollY > 100) {
-        backtotop.classList.add('active')
+        backtotop.classList.add("active");
       } else {
-        backtotop.classList.remove('active')
+        backtotop.classList.remove("active");
       }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
   }
 
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
 
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
 
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
+  on(
+    "click",
+    ".scrollto",
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
 
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
+        let navbar = select("#navbar");
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+        scrollto(this.hash);
       }
-      scrollto(this.hash)
-    }
-  }, true)
+    },
+    true
+  );
 
   /**
    * Scroll with ofset on page load with hash links in the url
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
-        scrollto(window.location.hash)
+        scrollto(window.location.hash);
       }
     }
   });
@@ -152,106 +164,94 @@
   /**
    * Preloader
    */
-  let preloader = select('#preloader');
+  let preloader = select("#preloader");
   if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
+    window.addEventListener("load", () => {
+      preloader.remove();
     });
   }
 
   /**
-   * Initiate  glightbox 
+   * Initiate  glightbox
    */
   const glightbox = GLightbox({
-    selector: '.glightbox'
+    selector: ".glightbox",
   });
 
- 
   /**
    * Animation on scroll
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
       easing: "ease-in-out",
       once: true,
-      mirror: false
+      mirror: false,
     });
   });
-
-})()
-
+})();
 
 //Announcement section
-function show_ann()
-{
- document.getElementById("ex").style.display="none";
-    document.getElementById("example").style.display="block";
+function show_ann() {
+  document.getElementById("ex").style.display = "none";
+  document.getElementById("example").style.display = "block";
 }
-function hide_ann()
-{
- document.getElementById("example").style.display="none";
-    document.getElementById("ex").style.display="block";
+function hide_ann() {
+  document.getElementById("example").style.display = "none";
+  document.getElementById("ex").style.display = "block";
 }
 
-       /*
+/*
         Countdown section
         */
 
-        (function () {
-          const second = 1000,
-                minute = second * 60,
-                hour = minute * 60,
-                day = hour * 24;
-        
-          let birthday = "Oct 01, 2021 09:00:00",
-              countDown = new Date(birthday).getTime(),
-              x = setInterval(function() {    
-        
-                let now = new Date().getTime(),
-                    distance = countDown - now;
-        
-                    var a,b,c,d;
-                   a= Math.floor(distance / (day));
-                   b= Math.floor((distance % (day)) / (hour));
-                   c= Math.floor((distance % (hour)) / (minute));
-                   d=Math.floor((distance % (minute)) / second);
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
-                   if(a<10 && a>=0)
-                        document.getElementById("days").innerText="0"+a;
-                   else
-                        document.getElementById("days").innerText=a;
+  let birthday = "Oct 01, 2021 09:00:00",
+    countDown = new Date(birthday).getTime(),
+    x = setInterval(function () {
+      let now = new Date().getTime(),
+        distance = countDown - now;
 
-                    if(b<10 && b>=0)
-                        document.getElementById("hours").innerText="0"+b;
-                   else
-                        document.getElementById("hours").innerText=b;     
+      var a, b, c, d;
+      a = Math.floor(distance / day);
+      b = Math.floor((distance % day) / hour);
+      c = Math.floor((distance % hour) / minute);
+      d = Math.floor((distance % minute) / second);
 
-                    if(c<10 && c>=0)
-                        document.getElementById("minutes").innerText="0"+c;
-                   else
-                        document.getElementById("minutes").innerText=c;
+      if (a < 10 && a >= 0) document.getElementById("days").innerText = "0" + a;
+      else document.getElementById("days").innerText = a;
 
-                    if(d<10 && d>=0)
-                        document.getElementById("seconds").innerText="0"+d;
-                   else
-                        document.getElementById("seconds").innerText=d;
+      if (b < 10 && b >= 0)
+        document.getElementById("hours").innerText = "0" + b;
+      else document.getElementById("hours").innerText = b;
 
-              /*  document.getElementById("days").innerText = Math.floor(distance / (day)),
+      if (c < 10 && c >= 0)
+        document.getElementById("minutes").innerText = "0" + c;
+      else document.getElementById("minutes").innerText = c;
+
+      if (d < 10 && d >= 0)
+        document.getElementById("seconds").innerText = "0" + d;
+      else document.getElementById("seconds").innerText = d;
+
+      /*  document.getElementById("days").innerText = Math.floor(distance / (day)),
                   document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
                   document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
                   document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second); */
-        
-                //do something later when date is reached
-                if (distance < 0) {
-                  
-                  document.getElementById("days").innerHTML="00";
-                  document.getElementById("hours").innerHTML="00";
-                  document.getElementById("minutes").innerHTML="00";
-                  document.getElementById("seconds").innerHTML="00";
-        
-                  clearInterval(x);
-                }
-                //seconds
-              }, 0)
-          }()); 
+
+      //do something later when date is reached
+      if (distance < 0) {
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+
+        clearInterval(x);
+      }
+      //seconds
+    }, 0);
+})();
